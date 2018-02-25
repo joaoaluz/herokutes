10b383972192b160d3ce638ea10f74cb552affad
joaoaluz <joao_aluz@hotmail.com>
2018-02-25 10:37:24 -0300

    s

--- manage.py
+++ manage.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             import django
         except ImportError:
             raise ImportError(
-                "Couldn'st import Django. Are you sure it's installed and "
+                "Couldn'sst import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
             )
